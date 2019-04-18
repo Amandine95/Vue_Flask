85db3b36d37ad5cd07961d5055a15279256b228e
liyixin <liyixin@ac361.com>
2019-04-18 17:08:25 +0800

    init commit

---
+++ fronted/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="f1">
+		<p>{{name}}</p>
+		<p>{{age}}</p>
+		<p>{{fun()}}</p>
+		<p>hello</p>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+			name:'sam',
+			age:18,
+			}
+			
+		},
+		methods:{
+			fun:function(){
+				return this.name + ' good!!';
+			}
+		}
+	
+	}
+</script>
+
+
